9fbb88f2119303b107a8b2665b4a2790dfa34418
Andrew Romanenco <andrew@romanenco.com>
2025-03-10 23:02:43 -0400

    Add smoke test for the parser

---
+++ tests/parser/parser.test.ts
@@ -0,0 +1,40 @@
+import LuaLexer from "@src/parser/LuaLexer";
+import LuaParser, { Exp_numberContext } from "@src/parser/LuaParser";
+import LuaParserListener from "@src/parser/LuaParserListener";
+import { CharStreams, CommonTokenStream, ParseTreeWalker } from "antlr4";
+
+class TestListener extends LuaParserListener {
+
+    public callBackWasCalled = false;
+
+    enterExp_number = (ctx: Exp_numberContext): void => {
+        this.callBackWasCalled = true;
+    }
+
+}
+
+test("parser smoke test: expression is called", () => {
+    const charStream = CharStreams.fromString('foo = 1;');
+    const lexer = new LuaLexer(charStream);
+    const tokenStream = new CommonTokenStream(lexer);
+    const parser = new LuaParser(tokenStream);
+    const ruleContext = parser.block();
+    
+    const testListener = new TestListener();
+    ParseTreeWalker.DEFAULT.walk(testListener, ruleContext);
+
+    expect(testListener.callBackWasCalled).toBe(true);
+});
+
+test("parser smoke test: expression is NOT alled", () => {
+    const charStream = CharStreams.fromString('some_function();');
+    const lexer = new LuaLexer(charStream);
+    const tokenStream = new CommonTokenStream(lexer);
+    const parser = new LuaParser(tokenStream);
+    const ruleContext = parser.block();
+    
+    const testListener = new TestListener();
+    ParseTreeWalker.DEFAULT.walk(testListener, ruleContext);
+
+    expect(testListener.callBackWasCalled).toBe(false);
+});
