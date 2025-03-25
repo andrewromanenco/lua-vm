f2cbdc7594a476e9b4c4941b75ff0af21e349873
Andrew Romanenco <andrew@romanenco.com>
2025-03-25 05:43:37 -0400

    Add utility to convert Lua code to a parsed object
    This utility creates a parser object that exposes nodes according
    to the Lua grammar.
    The parser represents the overall program structure with an expression,
    in addition to chunk and statement nodes.
    
    This will help with analyzing and manipulating Lua code programmatically.

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,10 @@
-export function add(a: number, b: number): number {
-    return a + b;
+import { CharStreams, CommonTokenStream, ParseTreeWalker } from "antlr4";
+import LuaLexer from "./parser/LuaLexer";
+import LuaParser from "./parser/LuaParser";
+
+export function make_parser(lua_code: string): LuaParser {
+    const charStream = CharStreams.fromString(lua_code);
+    const lexer = new LuaLexer(charStream);
+    const tokenStream = new CommonTokenStream(lexer);
+    return new LuaParser(tokenStream);
 }
--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,8 @@
-import { add } from "@src/utils";
+import LuaParser from "@src/parser/LuaParser";
+import { make_parser } from "@src/utils";
 
-test("adds two numbers", () => {
-  expect(add(2, 3)).toBe(5);
+test("validate parser", () => {
+  const parser = make_parser("10+5");
+  expect(parser).toBeInstanceOf(LuaParser);
+  expect(parser.exp).toBeTruthy();
 });
