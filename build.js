139ea94286d4daffb57fdcf1c967f8fcbbffbc3c
Andrew Romanenco <andrew@romanenco.com>
2025-04-12 00:09:02 -0400

    Add esbuild to produce a one file library

---
+++ build.js
@@ -0,0 +1,11 @@
+const esbuild = require('esbuild');
+
+esbuild.build({
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  outfile: 'dist/lua-vm.js',
+  format: 'iife',
+  globalName: 'LuaVM',
+  platform: 'browser',
+  target: ['es6'],
+}).catch(() => process.exit(1));
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { VMBuilder } from './vm';
