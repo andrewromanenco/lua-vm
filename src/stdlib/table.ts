25e2d0090738b1431db57bcdce2b5aff7fa0166e
Andrew Romanenco <andrew@romanenco.com>
2025-04-22 13:44:56 -0400

    Add stdlib table insert fuction

--- src/stdlib/table.ts
+++ src/stdlib/table.ts
@@ -10,8 +10,8 @@ import {
 import { getOrNil } from '@src/interpreter/utils';
 
 function concat(args: Value[]): Value[] {
-  const list = getOrNil(args, 0);
-  if (!(list instanceof TableValue)) {
+  const table = getOrNil(args, 0);
+  if (!(table instanceof TableValue)) {
     throw new ExtFunctionError('concat requires first argument to be a table');
   }
   const sep = getOrNil(args, 1);
@@ -30,25 +30,66 @@ function concat(args: Value[]): Value[] {
       throw new ExtFunctionError('concat requires index to be a number');
     }
   }
-  const i = iArg instanceof NilValue ? 1 : iArg.number;
-  const j = jArg instanceof NilValue ? lastIndex(list) : jArg.number;
+  const list = tableToList(table);
+  const i = iArg instanceof NilValue ? 0 : iArg.number - 1;
+  const j = jArg instanceof NilValue ? list.length - 1 : jArg.number - 1;
   const resultList = [];
   for (let k = i; k <= j; k++) {
-    resultList.push(list.get(NumberValue.from(k)).toString());
+    resultList.push(list[k].toString());
   }
   return [StringValue.from(resultList.join(sep.string))];
 }
 
-function lastIndex(table: TableValue): number {
+function tableToList(table: TableValue): Value[] {
+  const result = [];
   let i = 1;
   while (!(table.get(NumberValue.from(i)) instanceof NilValue)) {
+    result.push(table.get(NumberValue.from(i)));
     i++;
   }
-  return i - 1;
+  return result;
+}
+
+function insert(args: Value[]): Value[] {
+  const table = getOrNil(args, 0);
+  if (!(table instanceof TableValue)) {
+    throw new ExtFunctionError('table has to be provided');
+  }
+  const list = tableToList(table);
+  const a = getOrNil(args, 1);
+  const b = getOrNil(args, 2);
+  if (a instanceof NumberValue) {
+    const pos = a.number;
+    if (pos < 1 || pos > list.length + 1) {
+      throw new ExtFunctionError(
+        `pos has to be in [1..${list.length + 1}] inteval`
+      );
+    }
+    if (b instanceof NilValue) {
+      throw new ExtFunctionError("value can't be nil");
+    }
+    list.splice(pos - 1, 0, b);
+    return [listToTable(table, list)];
+  } else {
+    const pos = list.length;
+    if (a instanceof NilValue) {
+      throw new ExtFunctionError("value can't be nil");
+    }
+    list.splice(pos, 0, a);
+    return [listToTable(table, list)];
+  }
+}
+
+function listToTable(table: TableValue, list: Value[]): TableValue {
+  for (let i = 0; i < list.length; i++) {
+    table.set(NumberValue.from(i + 1), list[i]);
+  }
+  return table;
 }
 
 const functions = new TableValue();
 functions.set(StringValue.from('concat'), ExtFunction.of(concat));
+functions.set(StringValue.from('insert'), ExtFunction.of(insert));
 
 const tableStdLib = new TableValue();
 tableStdLib.set(StringValue.from('table'), functions);
--- tests/stdlib/table.test.ts
+++ tests/stdlib/table.test.ts
@@ -15,3 +15,25 @@ test('concat', () => {
   expectToBeString(result.globalVar('c3'), 'b,c');
   expectToBeString(result.globalVar('c4'), '');
 });
+
+test('insert', () => {
+  const lua = `
+    table1 = table.insert({"a", "b", "c"}, "d");
+    t1 = table.concat(table1, ",")
+    table2 = table.insert({}, "d");
+    t2 = table.concat(table2, ",")
+    table3 = table.insert({"a", "b", "c"}, 1, "d");
+    t3 = table.concat(table3, ",")
+    table4 = table.insert({"a", "b", "c"}, 3, "d");
+    t4 = table.concat(table4, ",")
+    table5 = table.insert({"a", "b", "c"}, 4, "d");
+    t5 = table.concat(table5, ",")
+    `;
+  const vm = new VMBuilder().witStdLib().build();
+  const result = vm.executeOnce(lua);
+  expectToBeString(result.globalVar('t1'), 'a,b,c,d');
+  expectToBeString(result.globalVar('t2'), 'd');
+  expectToBeString(result.globalVar('t3'), 'd,a,b,c');
+  expectToBeString(result.globalVar('t4'), 'a,b,d,c');
+  expectToBeString(result.globalVar('t5'), 'a,b,c,d');
+});
